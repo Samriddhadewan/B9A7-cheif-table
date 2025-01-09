fc3eb9b46a6266849eb48364a9cc77cdbf2f3bb0
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-09 23:56:04 +0600

    interactive section here

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ function App() {
     <div className='container mx-auto' >
     <Header></Header>
     <Banner></Banner>
+    <main>
+      <div className='text-center mt-16'>
+        <h1 className='#150B2B text-5xl font-semibold '>Our Recipes</h1>
+        <p className='#150B2B99 text-base font-normal my-3'>Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget <br /> urna volutpat curabitur elementum mauris aenean neque. </p>
+      </div>
+    </main>
+
     </div>
     </>
   )
